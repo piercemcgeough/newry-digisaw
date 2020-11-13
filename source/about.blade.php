e3debef0a489f166df1089fb7081c7e20a9b0f24
Pierce McGeough <pmcgeough@hotmail.co.uk>
2020-11-13 12:13:53 +0000

    initial commit

---
+++ source/about.blade.php
@@ -0,0 +1,6 @@
+@extends('_layouts.master')
+
+@section('body')
+<h1>about</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit temporibus dolore debitis voluptates nobis excepturi explicabo tempora hic sed, minus perferendis id ab nisi in quas tempore dicta. Quis, facere!</p>
+@endsection
